146a256981aa3e1fa93bded81932656251fd9302
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2023-03-25 22:43:22 +0500

    Basic Ui processing

---
+++ lib/UI/Screens/new_chat_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class ContactListScreen extends StatefulWidget {
+  const ContactListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ContactListScreen> createState() => _ContactListScreenState();
+}
+
+class _ContactListScreenState extends State<ContactListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
